480c5dcbfa877127dbca50dd4ae94942fb09af1a
Evan <evanshanges@gmail.com>
2017-02-05 01:00:12 -0500

    home/login/signup done

--- component/home/homeService.js
+++ component/home/homeService.js
@@ -3,6 +3,15 @@
  */
 angular.module('grafie.home').controller('homeController', function($stateParams, $scope, $rootScope){
 
+    $(document).ready(function(){
+        console.log($stateParams);
+        if($stateParams.type =='login'){
+            console.log('lol');
+            $('#homeInfo').hide();
+            $('#homeLogin').fadeIn('fast', 'linear')
+        }
+    });
+
     $scope.switchLogDiv = function (){
         $('#homeInfo').fadeOut('fast', 'linear');
         $('#homeLogin').fadeIn('slow', 'linear');
